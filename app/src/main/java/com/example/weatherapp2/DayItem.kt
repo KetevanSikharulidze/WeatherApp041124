9183e61f3d1ad452f0bb147605cd11e7d0d7eecc
justKeth <sixaruliketi@gmail.com>
2024-11-04 10:47:21 +0400

    added until vp adapter initialization

---
+++ app/src/main/java/com/example/weatherapp2/DayItem.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp2
+
+data class DayItem (
+    val city: String,
+    val temp: String,
+    val condition: String,
+    val imgURL: String,
+    val date: String,
+    val minMaxTemp: String,
+    val hours: String
+)
